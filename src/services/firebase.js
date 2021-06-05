1ac31784722d49f7a2334a7e08257f049ccf8cbd
joao <jprp17@live.com>
2021-06-05 15:37:02 -0300

    refactor: change firebase config variables and set it as .en variables

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,13 +2,12 @@ import firebase from 'firebase';
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyCgL0ZHTQpYP0SU9iYzQ6FWmcBbf-FtWXQ",
-  authDomain: "bookreview-56c7d.firebaseapp.com",
-  databaseURL: "https://bookreview-56c7d.firebaseio.com",
-  projectId: "bookreview-56c7d",
-  storageBucket: "bookreview-56c7d.appspot.com",
-  messagingSenderId: "947076511336",
-  appId: "1:947076511336:web:06e8adf6a85274244c6ddd"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_ID
 });
 
 // Database
